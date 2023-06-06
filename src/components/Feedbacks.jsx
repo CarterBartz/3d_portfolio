a5f4168fe13b1c638f2973054f5b31e9866aea34
CarterBartz <bartz196@d.umn.edu>
2023-06-06 00:32:58 -0500

    Testamonials color updated

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -8,15 +8,15 @@ import { testimonials } from '../constants';
 const FeedbackCard = ({ index, testimonial, name, designation, company, image }) => (
   <motion.div 
     varients={fadeIn("", "spring", index * 0.5, 0.75)}
-    className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
+    className="bg-tertiary p-10 rounded-3xl xs:w-[320px] w-full"
   >
     <p className="text-white font-black text-[48px]">"</p>
 
     <div className="mt-1">
       <p className="text-white tracking-wider text-[18px]">{testimonial}</p>
 
-      <div className="mt-7 flex justify-between tiems-center gap-1">
-        <div className="flex-1 flex flex-col">
+      <div className="flex justify-between gap-1 mt-7 tiems-center">
+        <div className="flex flex-col flex-1">
           <p className="text-white font-medium text-[16px]">
             <span className="blue-text-gradient">@</span> {name}
           </p>
@@ -28,7 +28,7 @@ const FeedbackCard = ({ index, testimonial, name, designation, company, image })
         <img 
           src={image}
           alt={`feedback-by-${name}`}
-          className="w-10 h-10 rounded-full object-cover"
+          className="object-cover w-10 h-10 rounded-full"
         />
 
       </div>
@@ -39,8 +39,8 @@ const FeedbackCard = ({ index, testimonial, name, designation, company, image })
 
 const Feedbacks = () => {
   return (
-    <div className="mt-12 bg-black-100 rounded-[20px]">
-      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+    <div className="mt-12 bg-grey-light rounded-[20px]">
+      <div className={`${styles.padding} bg-grey-mid rounded-2xl min-h-[300px]`}>
         <motion.div varients={textVariant()}>
           <p className={styles.sectionSubText}>What otehrs say</p>
           <h2 className={styles.sectionHeadText}>Testimonials.</h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,12 @@ module.exports = {
       colors: {
         primary: "#0000",
         secondary: "#84d0cf",
-        tertiary: "rgb(74, 74, 74)",
+        tertiary: "#4a4a4a",
         "black-100": "#100d25",
         "black-200": "#090325",
         "white-100": "#f3f3f3",
+        "grey-light": "#a3a3a3",
+        "grey-mid": "#5e5e5e",
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
