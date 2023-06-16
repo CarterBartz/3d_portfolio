e96546eb0ee24aeb500d32ba0bceb16c60b2574f
CarterBartz <bartz196@d.umn.edu>
2023-06-16 00:23:01 -0500

    added testimonial to contact gradient

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,10 @@ const App = () => {
         <Works />
         <Feedbacks />
         <div className="relative z-0">
-          <Contact />
-          <StarsCanvas />
+          <div className="contact-gradient-pattern">
+            <Contact />
+            <StarsCanvas />
+          </div>
         </div>
       </div>
     </BrowserRouter>
